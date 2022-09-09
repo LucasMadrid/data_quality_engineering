eb7eb1c5146fa20ef9f5824964a7468281291628
Lucas Madrid <lucas.madrid@ttec.com>
2022-09-09 10:18:23 -0300

    Add Docstring and MODIFY README

--- README.md
+++ README.md
@@ -1,19 +1,21 @@
 # Data Quality Engineering
+
 This projects it meant to create a process that consume data files, check through that files if they are already processed.
 After that it going to clean the data an write it down as _out/_bad data csv depending if the location column has a valid value.
 
-### Technologies
-* Python
-* Bash
-* PySpark
-* Pydeequ 
+## Technologies
+
+* Python.
+* Bash.
+* PySpark.
+* Pydeequ. [Pydeequ Github](https://github.com/awslabs/python-deequ)
 
 ### Setting Up
+
 1. ```python --verison```
 2. ```pip install pipenv``` installing pipenv package to manage environments.
 3. ```pipenv install``` is going to install all dependencies in pipfiles.
 4. ```./file_check.sh``` check file on ./data folder and execute python script to clean and validate unprocessed data files.
-
 
 ### Process Diagram
 
--- data_check_module/data_validation.py
+++ data_check_module/data_validation.py
@@ -2,9 +2,24 @@ from pydeequ.checks import *
 from pydeequ.verification import *
 
 
-def validate_data(df): 
-    check = Check(spark, CheckLevel.Warning, "Review Check")
+def validate_data(df):
 
+    """
+    Perform data validation with PyDeequ on the Spark DataFrame. 
+    More info: https://github.com/awslabs/python-deequ
+
+    Parameters
+    ----------
+    df: Spark DataFrame 
+        Dataset cleaned and ready to be validated.
+
+    Returns
+    -------
+    None
+
+    """
+
+    check = Check(spark, CheckLevel.Warning, "Review Check")
     checkResult_votes_column = VerificationSuite(spark) \
         .onData(df) \
         .addCheck(
@@ -26,6 +41,5 @@ def validate_data(df):
         
             ) \
         .run()
-
     checkResult_df = VerificationResult.checkResultsAsDataFrame(spark, checkResult_votes_column)
     checkResult_df.show(truncate=True)
